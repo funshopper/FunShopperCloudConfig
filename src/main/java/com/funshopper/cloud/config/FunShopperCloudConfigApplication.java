93c6a01106409c4592ca01ab8eada7efb252c6b0
Gabriel Kigundu <gkigundu>
2019-07-11 00:37:10 -0400

    Added config server and eureka properties

---
+++ src/main/java/com/funshopper/cloud/config/FunShopperCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package com.funshopper.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunShopperCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunShopperCloudConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funshopper/cloud/config/FunShopperCloudConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.funshopper.cloud.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FunShopperCloudConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
